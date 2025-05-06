9a33d94ba22435934be0521b6b4b59a26207f19d
Lucas_Bonde <111398229+LucasBondeDeSouza@users.noreply.github.com>
2025-05-06 14:29:38 -0300

    atualização

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Round from "./components/Round";
 
 export default () => {
   const [data, setData] = useState([]);
-  const [round, setRound] = useState(7)
+  const [round, setRound] = useState(8)
   const [scores, setScores] = useState({});
 
   const fetchMatches = async () => {
